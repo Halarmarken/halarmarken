1dfa13fad757ed505c49cc71afcae777167d8e5e
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2024-07-28 14:40:27 +0300

    initial next setup

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {},
+  plugins: []
+};
+
+export default config;
